6c2b20ef24cf0d48160c46c41cc97fbb48cf4ca1
crate <crate@LAPTOP-M5R408BR>
2019-11-16 22:46:09 +0100

    Constructor parametrado

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -1,35 +1,62 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Posicion {
-	
+
 	private int fila;
 	private char columna;
-	
-	
+
+
 	public int getFila() {
 		return fila;
 	}
 	private void setFila(int fila) {
 
 		if(fila<1 || fila>8){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("ERROR: Fila no válida");
 		} else {
 
 			this.fila = fila;
 		}
 
 	}
+
 	public char getColumna() {
 		return columna;
 	}
+
 	private void setColumna(char columna) {
-		
-		if(columna < 'a' || columna > 'h'){
-			throw new IllegalArgumentException();
+
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida");
 		} else {
 			this.columna = columna;
 		}
 	}
+	
+	/*Constructor*/
+	
+
+	Posicion(int fila,char columna){
+
+		if(fila<1 || fila>8){
+			throw new IllegalArgumentException("ERROR: Fila no válida");
+		} else {
+
+			this.setFila(fila);
+		}
+
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida");
+		} else {
+			this.setColumna(columna);
+		}
+
+
+	}
+
+
+	
+	
 	
 
 }
